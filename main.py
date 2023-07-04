dd0a7df90a413506637a72b3ba2ea886703fe31a
JointP <morozoff-15@yandex.ru>
2023-07-04 18:44:13 +0300

    add Task #3

--- main.py
+++ main.py
@@ -16,9 +16,21 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_rating_s(self):
+        count = 0
+        if len(self.grades.values()) == 0:
+            return 'Ошибка'
+        else:
+            for rates_s in self.grades.values():
+                for rate_s in rates_s:
+                    count += rate_s
+                    average_rate_s = count / len(rates_s)
+                return average_rate_s
+
     def __str__(self):
-        res = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {self.grades}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}')
+        res = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашнее задание: {self.average_rating_s()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}')
         return res
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -33,8 +45,19 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades_l = {}
 
+    def average_rating_l(self):
+        count = 0
+        if len(self.grades_l.values()) == 0:
+            return 'Ошибка'
+        else:
+            for rates_l in self.grades_l.values():
+                for rate_l in rates_l:
+                    count += rate_l
+                    average_rate_l = count / len(rates_l)
+                return average_rate_l
+
     def __str__(self):
-        res = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.grades_l}')
+        res = (f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating_l()}')
         return res
 
 class Reviewer(Mentor):
@@ -60,16 +83,19 @@ class Reviewer(Mentor):
 
 student_1 = Student('Bill', 'Tobin', 'male')
 student_1.courses_in_progress += ['Python']
+student_1.courses_in_progress += ['Git']
 student_1.finished_courses += ['Designer']
 student_1.finished_courses += ['Tester']
 
 student_2 = Student('Panther', 'Black', 'female')
 student_2.courses_in_progress += ['Python']
+student_2.courses_in_progress += ['Git']
 student_2.finished_courses += ['Designer']
 student_2.finished_courses += ['Java']
 
 student_3 = Student('Tyler', 'Creator', 'male')
 student_3.courses_in_progress += ['Python']
+student_3.courses_in_progress += ['Git']
 student_3.finished_courses += ['Tester']
 student_3.finished_courses += ['Java']
 
@@ -88,10 +114,14 @@ reviewer_1.courses_attached += ['Java']
 
 reviewer_2 = Reviewer('David', 'Smith')
 reviewer_2.courses_attached += ['Designer']
+reviewer_2.courses_attached += ['Python']
 
 reviewer_1.rate_hw(student_1, 'Python', 10)
 reviewer_1.rate_hw(student_2, 'Python', 9)
 reviewer_1.rate_hw(student_3, 'Python', 7)
+reviewer_2.rate_hw(student_1, 'Python', 9)
+reviewer_2.rate_hw(student_2, 'Python', 8)
+reviewer_2.rate_hw(student_3, 'Python', 9)
 
 
 print(student_1)
